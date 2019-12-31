2a7825831d4438966472dfbee16735c7afe79b24
Pavel <pavelsobolev1991@gmail.com>
2019-12-31 13:45:56 +0300

    Updates

---
+++ precache-manifest.c37dbd495d6126ea784b337afda7a2b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b7d6fd53acd3202af511b5b13cb05b0",
+    "url": "/to-do-list-react/index.html"
+  },
+  {
+    "revision": "f40edb2ca9e005e85433",
+    "url": "/to-do-list-react/static/css/main.d1121c44.chunk.css"
+  },
+  {
+    "revision": "18df49a6b88576af5ec2",
+    "url": "/to-do-list-react/static/js/2.5a84b7b7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/to-do-list-react/static/js/2.5a84b7b7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f40edb2ca9e005e85433",
+    "url": "/to-do-list-react/static/js/main.73afe583.chunk.js"
+  },
+  {
+    "revision": "7562a128e6eb3edd321d",
+    "url": "/to-do-list-react/static/js/runtime-main.a85cec3c.js"
+  }
+]);
